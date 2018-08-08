ab48b63faf381e6eb6607155c2a3decc832f9445
TomaOmega <tomaomega@gmail.com>
2018-08-08 21:48:43 +0200

    windowsize tesz with animation

--- core/src/com/haramiak/main/Launcher.java
+++ core/src/com/haramiak/main/Launcher.java
@@ -7,27 +7,58 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Launcher extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    SpriteBatch batch;
+    Texture img;
+    private int imgX, imgY;
+    private boolean incrementX, incrementY;
+
+    @Override
+    public void create() {
+        imgX = 0;
+        imgY = 50;
+        incrementX = false;
+        incrementY = false;
+        batch = new SpriteBatch();
+        img = new Texture("badlogic.jpg");
+    }
+
+    private void update() {
+        if (imgX == Gdx.app.getGraphics().getWidth()) {
+            incrementX = false;
+        } else if (imgX == 0) {
+            incrementX = true;
+        }
+        if (imgY == Gdx.app.getGraphics().getHeight()) {
+            incrementY = false;
+        } else if (imgY == 0) {
+            incrementY = true;
+        }
+        if (incrementX) {
+            imgX++;
+        } else {
+            imgX--;
+        }
+        if (incrementY) {
+            imgY++;
+        } else {
+            imgY--;
+        }
+    }
+
+    @Override
+    public void render() {
+        update();
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(img, imgX, imgY);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        img.dispose();
+    }
 }
