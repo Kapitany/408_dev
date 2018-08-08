5ac45dcd97aa473fcf74ef9c4d4fe307a69f79fd
Kapitany <tuzcsapas94@freemail.hu>
2018-08-08 20:16:46 +0200

    Generated lib files

---
+++ desktop/src/com/408/main/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.408.main.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.408.main.Launcher;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Launcher(), config);
+	}
+}
